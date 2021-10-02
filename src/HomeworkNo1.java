0ab9fa93fb3aa86e3b5afc4eeeb033b6551193a3
Avetis Khachatryan <avetis@synopsys.com>
2021-10-02 09:19:42 +0400

    First independent program!

---
+++ src/HomeworkNo1.java
@@ -0,0 +1,35 @@
+public class HomeworkNo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Avetis");
+        int a =76;
+        int b =1190;
+        int c = a+b;
+        System.out.println(c);
+        int d =834;
+        int e =2791;
+        int f = d+e;
+        System.out.println("834+2791= " + f);
+        int g = 246/5;
+        System.out.println(g);
+        int h =1005;
+        int i = 28;
+        int j = h/i;
+        System.out.println(j);
+        a = -5 + 8 * 6;
+        System.out.println(a);
+        b = (55+9) % 9;
+        System.out.println(b);
+        c = 20 + -3*5 / 8;
+        System.out.println(c);
+        d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(d);
+        char A = '\u0531';
+        char V = '\u057E';
+        char E = '\u0565';
+        char T = '\u057F';
+        char I = '\u056B';
+        char S = '\u057D';
+        System.out.println("My name is : " + A + V + E + T + I + S);
+    }
+}
